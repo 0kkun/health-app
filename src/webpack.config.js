80b6293d209001f7e2e9cc2d1bae568e0d096193
0kkun <shinji.okumoto.01@gmail.com>
2021-06-28 16:15:25 +0900

    read bundle.js

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
     entry: './html/js/dist/main.js', // 読み込むメインのファイル
     output: {
-        filename: '[contenthash]bundle.js', // 出力後のファイル名
+        filename: 'bundle.js', // 出力後のファイル名
         path: path.resolve(__dirname, 'html', 'js', 'dist') //　出力先。絶対パスで指定する
     },
     mode: 'production'

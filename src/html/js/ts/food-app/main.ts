1969ee8b5751a0e1c1c1b3bbf84bb6099698cc5b
0kkun <shinji.okumoto.01@gmail.com>
2021-06-29 10:51:54 +0900

    add api test class

--- src/html/js/ts/food-app/main.ts
+++ src/html/js/ts/food-app/main.ts
@@ -1,3 +1,8 @@
 import { Foods } from "./foods";
+import { ApiGetTest } from './api-test';
 
 Foods.getInstance();
+
+// APIテスト用
+const apiTestUrl = "https://randomuser.me/api/";
+ApiGetTest(apiTestUrl);
